2b292f82128515b4e8b4d23376da4e51121e0def
Max Moser <144325833+maxmoser07@users.noreply.github.com>
2025-04-07 15:12:31 +0200

    Add files via upload

--- console_1.sql
+++ console_1.sql
@@ -6,3 +6,6 @@ db.emps.find({dept_id:10},{_id:0,ENAME:1, JOB:1, HIREDATE:1});
 
 // 3. Beispiel
 db.emps.find({JOB:"CLERK"},{_id:0,ENAME:1, JOB:1, SAL:1});
+
+// 4. Beispiel
+db.emps.find({dept_id:{$ne:10}},{_id:0});
