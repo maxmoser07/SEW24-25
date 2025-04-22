84d19ae7212d3085c14e3df89ee091cc87a8ca59
maxmoser07 <m.moser@htlkrems.at>
2025-04-22 09:08:09 +0200

    bomben zählen funktioniert

--- BlazorTTT/BlazorTTT/Minesweeper.cs
+++ BlazorTTT/BlazorTTT/Minesweeper.cs
@@ -33,9 +33,39 @@ public class MineSweeper
         {
             for (int j = 0; j < _field_.GetLength(1); j++)
             {
-                _field_[i, j] = rand.Next(0, 2);
+                if (rand.Next(0, 9) == 5)
+                {
+                    _field_[i, j] = 9;
+                }
             }
         }
+        SetClues();
     }
+
+    public void SetClues()
+    {
+        for (int i = 0; i < _field_.GetLength(0); i++)
+        {
+            for (int j = 0; j < _field_.GetLength(1); j++)
+            {
+                if (_field_[i, j] == 9)
+                {
+                    for (int x = i - 1; x <= i + 1; x++)
+                    {
+                        for (int y = j - 1; y <= j + 1; y++)
+                        {
+                            if (x >= 0 && x < _field_.GetLength(0) && y >= 0 && y < _field_.GetLength(1))
+                            {
+                                if (_field_[x, y] != 9)
+                                {
+                                    _field_[x, y]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } 
     
 }
