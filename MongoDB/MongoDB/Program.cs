529cdfb9bfaa4742681732ce6c87828cf2cc1453
maxmoser07 <m.moser@htlkrems.at>
2025-05-20 08:25:03 +0200

    first access to mongo database

---
+++ MongoDB/MongoDB/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MongoDB;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+/*
+Person person1 = new Person
+{
+    Id = 1,
+    Name = "John Doe",
+    Age = 30
+};
+Console.WriteLine();
+foreach (var propertyInfo in typeof(Person).GetProperties())
+{
+    Console.WriteLine(propertyInfo.Name +";" + propertyInfo.GetValue(person1));
+}
+*/
+var client = new MongoClient("mongodb://localhost:27017");
+var database = client.GetDatabase("3chitmongodb");
+var collection = database.GetCollection<BsonDocument>("emps");
+
+var list = await collection.Find(new BsonDocument()).ToListAsync();
+
+foreach (var document in list)
+{
+    Console.WriteLine(document);
+}
